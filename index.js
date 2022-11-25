733be034c7c99449d5d541a7eef59d5f4d3cee53
tawhidhaider <tawhidalam69@gmail.com>
2022-11-25 15:16:10 +0600

    server testing

--- index.js
+++ index.js
@@ -15,3 +15,18 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@database1.wijxnwd.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run(){
+    try{
+
+    }
+    finally{
+
+    }
+
+}
+run().catch(console.log);
+
+app.get('/',(req,res)=>{
+    res.send("Welcome to gyan_vandar_server ")
+});
+app.listen(port,()=>console.log(`running on port${port}`))
